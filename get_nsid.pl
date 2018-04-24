f62c18b28a25efc184b29b52c952602bea69ad86
yuta fujii <innerflight0422@gmail.com>
2018-04-24 19:14:03 +0900

    Update get_nsid.pl

--- get_nsid.pl
+++ get_nsid.pl
@@ -1,1 +1,37 @@
 #!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Net::DNS;
+use Data::Dumper;
+
+my $qname = "example.com";
+my $type = "A";
+
+my $resolver = Net::DNS::Resolver->new(
+        'nameservers' => 'a.iana-servers.net',
+        'port'        => 53,
+        'retry'       => 3,
+        'udp_timeout' => 3,
+        'tcp_timeout' => 3,
+        'recurse'     => 0,
+);
+
+my $packet = new Net::DNS::Packet("$qname", "$type");
+
+my $rr = new Net::DNS::RR(
+        type    => 'OPT',
+        size    => 4960,
+        option => [ 'NSID' => '' ],
+);
+
+$packet->push(additional => $rr);
+my $reply = $resolver->send($packet);
+
+## debug
+#print Dumper $rep;
+
+if ($reply->additional){
+        my $nsid = $reply->pop("additional")->rdata;
+        print "NSID: $nsid\n"
+}
